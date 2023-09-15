c0d2ff6151f40b7451485e3c4325f9796bb13058
klmwook <klmwook72@gmail.com>
2023-09-15 12:19:34 +0900

    반복되는 모션 정보 배열로 관리

--- main.js
+++ main.js
@@ -1,20 +1,14 @@
 const wrap = document.querySelector('.wrap');
+const boxs = wrap.querySelectorAll('div');
 
-/*
-basic use
-gsap.set('.box', { x: -200, opacity: 0 });
-
-wrap.addEventListener('click', () => {
-	gsap.fromTo('.box', { x: -200, opacity: 0 }, { x: 0, opacity: 1 });
-});
-*/
+const motionInfo = [
+	{ x: 700, opacity: 1, duration: 2 },
+	{ x: 700, scale: 1.5, opacity: 1, duration: 0.5 },
+	{ x: 700, opacity: 1, rotation: 150, duration: 1.5 },
+	{ x: 700, opacity: 1, scale: 0.5, duration: 0.2 },
+	{ x: 700, opacity: 1, y: 300, duration: 1.8 },
+];
 
 //bg, transform-origin, duration, relativePosition
-wrap.addEventListener('click', () => {
-	gsap.to('div', {
-		duration: 1,
-		x: 700,
-		opacity: 1,
-		stagger: 0.2,
-	});
-});
+const tl = gsap.timeline();
+wrap.addEventListener('click', () => motionInfo.forEach((data, idx) => tl.to(boxs[idx], data)));
