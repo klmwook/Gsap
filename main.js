4fbfc5609decd55812fcd2aadaf3821ba415f09a
klmwook <klmwook72@gmail.com>
2023-09-15 12:18:55 +0900

    시간차 적용

--- main.js
+++ main.js
@@ -1,7 +1,20 @@
 const wrap = document.querySelector('.wrap');
 
+/*
+basic use
 gsap.set('.box', { x: -200, opacity: 0 });
 
 wrap.addEventListener('click', () => {
 	gsap.fromTo('.box', { x: -200, opacity: 0 }, { x: 0, opacity: 1 });
+});
+*/
+
+//bg, transform-origin, duration, relativePosition
+wrap.addEventListener('click', () => {
+	gsap.to('div', {
+		duration: 1,
+		x: 700,
+		opacity: 1,
+		stagger: 0.2,
+	});
 });
